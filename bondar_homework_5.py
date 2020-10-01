7b2f55a4b1dee25a878a90e8f7e46d54bfcdb77d
but_what_if <bondar.vladimir.all@gmail.com>
2020-10-01 16:24:58 +0300

    file created

---
+++ bondar_homework_5.py
@@ -0,0 +1,66 @@
+# task 1 ##############################
+
+# number = 405099608800
+# print(str(number).count('0'))
+
+
+
+
+
+
+# task 2 ##############################
+
+# number = 405099608800
+# str_num_reversed = str(number)[::-1]
+# zero_count = ''
+# for _ in str_num_reversed:
+#     if _ == '0':
+#         zero_count += _
+#     else:
+#         break
+# print(len(zero_count))
+
+
+
+
+
+# task 3 ##############################
+
+# my_list_1 = [1, 2, 3, 4, 5]
+# my_list_2 = [10, 15, 20, 25]
+# my_result = []
+# for sym in my_list_1:
+#     if sym % 2 == 0:
+#         my_result.append(sym)
+# for sym in my_list_2:
+#     if sym % 2 != 0:
+#         my_result.append(sym)
+# print(my_result)
+
+
+
+
+
+# task 4 ##############################
+
+# my_list = [1, 2, 3, 4]
+# new_list = []
+# new_list.extend(my_list[1:])
+# new_list.append(my_list[0])
+# print(new_list)
+
+
+
+
+
+
+
+# task 5 ##############################
+
+
+
+# task 6 ##############################
+# task 7 ##############################
+# task 8 ##############################
+# task 9 ##############################
+# task 10 #############################
