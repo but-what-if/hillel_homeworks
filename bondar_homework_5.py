2b2cbb20a6b3947339187ec7b182c0a6c2a089f9
but_what_if <bondar.vladimir.all@gmail.com>
2020-10-01 17:40:08 +0300

    file created

--- bondar_homework_5.py
+++ bondar_homework_5.py
@@ -57,10 +57,39 @@
 
 # task 5 ##############################
 
+# my_list = [1, 2, 3, 4]
+# my_list.append(my_list.pop(0))
+# print(my_list)
+
+
+
 
 
 # task 6 ##############################
+
+# my_str = '43 больше чем 34 но меньше чем 56'
+# my_result = 0
+# new_str = ''
+# for sym in my_str:
+#     if not sym.isalpha():
+#         new_str += sym
+# add_list_1 = new_str.split(' ')
+# add_list_2 = []
+# for index in range(len(add_list_1)):
+#     if add_list_1[index].isdigit():
+#         add_list_2.append(int(add_list_1[index]))
+# for value in add_list_2:
+#     my_result += value
+# print(my_result)
+
+
+
+
+
 # task 7 ##############################
+
+
+
 # task 8 ##############################
 # task 9 ##############################
 # task 10 #############################
