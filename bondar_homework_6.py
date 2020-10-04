b750b1327303ec61f1d50a60e9b2376482c19339
but_what_if <bondar.vladimir.all@gmail.com>
2020-10-04 14:27:05 +0300

    file created. 3 tasks done

---
+++ bondar_homework_6.py
@@ -0,0 +1,32 @@
+# task 1 ###############################################
+
+# my_list = ['qwe', 'qwe', 'qwe', 'qwe', 'qwe']
+# new_list = []
+# for index, value in enumerate(my_list):
+#     if index % 2 == 0:
+#         new_list.append(value)
+#     else:
+#         new_list.append(value[::-1])
+# print(new_list)
+
+
+
+
+# task 2 ##############################################
+
+# my_list = ['qwe', 'asd', 'fgh', 'aer', 'aty', 'uty']
+# new_list = [value for value in my_list if value[0] == 'a']
+# print(new_list)
+
+
+
+# task 3 ##############################################
+
+# my_list = ['qwe', 'asd', 'fgah', 'aer', 'aty', 'uty']
+# new_list = [value for value in my_list if 'a' in value]
+# print(new_list)
+
+
+
+# task 4 ##############################################
+
