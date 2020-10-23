ff51467563d824d6883b8e9420bcdd8a6a2af7f8
VolodymyrBondar <bondar.vladimir.all@gmail.com>
2020-10-23 20:13:57 +0300

    file changed.

--- bondar_homework_11/bondar_homework_11.py
+++ bondar_homework_11/bondar_homework_11.py
@@ -1,10 +1,48 @@
 import json
 
-
-def read_json_file(filename):
-    with open(filename, 'r') as json_file:
-        data = json.load(json_file)
-        print(data, type(data))
+path = 'data.json'
 
 
-read_json_file('data.json')
+def read_json_file(filename_with_path):
+    with open(filename_with_path, encoding='utf-8') as json_file:
+        return json.load(json_file)
+
+
+data = read_json_file(path)
+
+
+def get_rows(data_list):
+    for row in data_list:
+        return row
+
+
+def sort_surnames(dict_: dict):
+    name = dict_["name"].split(" ")[-1]
+    return name
+
+
+sorted_surnames_list = sorted(data, key=sort_surnames)
+
+
+def sort_death_date(dict_: dict):
+    year = dict_["years"].split("–")[1]
+    year = year.replace('c. ', '').replace('.', '')
+    if len(year.strip('.').split()) == 1:
+        return int(year.split()[0][:-1])
+    return -int(year.split(" ")[-2])
+
+
+sorted_death_date_list = sorted(data, key=sort_death_date)
+
+
+def sort_text(dict_: dict):
+    text = dict_["text"].split(" ")
+    return len(text)
+
+
+sorted_text_list = sorted(data, key=sort_text)
+
+
+
+
+
